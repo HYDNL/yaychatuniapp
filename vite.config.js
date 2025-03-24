18cf427e708ef766d62d67b12cb7593292fcbd28
HYDNL <153161197+HYDNL@users.noreply.github.com>
2025-03-24 13:15:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [vue(), commonjs()]
+  plugins: [vue()]
 });
